091d10f396e9af3be065228ffe3e64689c7d582d
supercandela <supercandela@gmail.com>
2023-09-27 23:37:05 -0300

    Agrego Proyecto Entidades

---
+++ MiCalculadora/ConsoleTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using Entidades;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Numeracion elemento = new Numeracion();
+
+            elemento.sistema = (Numeracion.ESistema)1;
+            elemento.valorNumerico = 13800.25;
+
+            Console.WriteLine($"El número es {elemento.Valor} y su sistema es {elemento.ConvertirA(elemento.sistema)}");
+        }
+    }
+}
---
+++ MiCalculadora/Entidades/Numeracion.cs
@@ -0,0 +1,61 @@
+﻿namespace Entidades
+{
+    public class Numeracion
+    {
+        public ESistema sistema;
+        public double valorNumerico;
+
+        /// <summary>
+        /// Propiedad Sistema. Retorna el ESistema en que debe representarse el valor numérico.
+        /// </summary>
+        public ESistema Sistema
+        {
+            get 
+            { 
+                return this.sistema; 
+            }
+        }
+        /// <summary>
+        /// Propiedad Valor. Retorna el valor del objeto de acuerdo al sistema de numeración de su propiedad Sistema.
+        /// </summary>
+        public string Valor
+        {
+            get
+            {
+                return this.valorNumerico.ToString();
+            }
+        }
+        /// <summary>
+        /// Convierte el valor de binario a decimal.
+        /// </summary>
+        /// <param name="valor">Valor a convertir.</param>
+        /// <returns>El valor convertido de string a double.</returns>
+        private double BinarioADecimal (string valor)
+        {
+            double valorDecimal;
+            if (!double.TryParse(valor, out valorDecimal))
+            {
+                valorDecimal = double.MinValue;
+            }
+            return valorDecimal;
+        }
+        /// <summary>
+        /// Retorna un string
+        /// </summary>
+        /// <param name="sistema">ESistema</param>
+        /// <returns></returns>
+        public string ConvertirA(ESistema sistema)
+        {
+            return sistema.ToString();
+        }
+        private string DecimalABinario(int valor)
+        {
+            return "Hola";
+        }
+        public enum ESistema
+        {
+            Decimal,
+            Binario
+        }
+    }
+}
